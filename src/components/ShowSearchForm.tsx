8d9471ba4c171db491a04b195466b9e7cffb89cc
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-04-11 21:42:16 +1000

    change action

--- src/components/ShowSearchForm.tsx
+++ src/components/ShowSearchForm.tsx
@@ -5,7 +5,7 @@ import { Field } from './Form/Field';
 export const ShowSearchForm: React.FC = () => {
   return (
     <Form
-      action="http://localhost:5000/shows/search"
+      action="http://localhost:5000/api/shows/search"
       render={() => (
         <>
           <Field id="name" label="Name" />
