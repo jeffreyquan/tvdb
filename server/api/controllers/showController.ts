7bb1e7a0b5d71d5172278564c99afdacbf5c414a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-04-11 21:41:54 +1000

    axios request to tmdb api

--- server/api/controllers/showController.ts
+++ server/api/controllers/showController.ts
@@ -1,13 +1,18 @@
 import Show from '../models/Show';
-import axios from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 export const listSearchResults = (req, res) => {
-  console.log(JSON.parse(req.query.values));
-  // axios
-  //   .get(
-  //     `https://api.themoviedb.org/3/search/tv?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1&query=${}&include_adult=false`
-  //   )
-  //   .then((res) => {
-  //     console.log(res);
-  //   });
+  let query = JSON.parse(req.query.values).name;
+  let data;
+  axios
+    .get(
+      `https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1&query=${ query }&include_adult=false`
+    )
+    .then((response: AxiosResponse) => {
+      data = response.data;
+      res.json(data);
+    })
+    .catch((err: AxiosError) => {
+      console.log(err);
+    });
 }
