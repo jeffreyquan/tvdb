49278e1b887ebcbcd22be4830cc7405a91fc1bc2
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-04-11 17:47:10 +1000

    testing show search route

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import showRouter from './api/routes/showRoutes';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/shows', showRouter);
 
 mongoose.Promise = global.Promise;
 
