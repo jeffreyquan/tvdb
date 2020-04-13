ebe57b5305ae6fd45ce0deb58cb6a568a657679f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-04-13 23:51:36 +1000

    added showtile to display each result from search

--- src/components/ShowSearchResults.tsx
+++ src/components/ShowSearchResults.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ResponseContext, IResponseContext } from './Form/Form';
+import { ShowTile } from './ShowTile';
 
 export const ShowSearchResults = () => {
   
@@ -7,8 +8,8 @@ export const ShowSearchResults = () => {
     <ResponseContext.Consumer>
       {(context: IResponseContext) =>
         Object.keys(context.response).length > 0 &&
-        context.response.results.map((result: any, index: number) => (
-          <div key={index}>{result.original_name}</div>
+        context.response.results.map((result: any) => (
+          <ShowTile key={result.id} tmdbId={result.id} name={result.original_name} genreIds={result.genre_ids} firstAirDate={result.first_air_date} overview={result.overview} poster={result.poster_path} />
         ))
       }
     </ResponseContext.Consumer>
