3602d5718a4246e550280b29eaf215ad4a18f877
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-04-27 22:36:24 +1000

    set shows in local state and display via map function

--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ShowCard } from "../components/ShowCard";
+import { IShow } from "../types";
+import { CircularProgress } from "@material-ui/core";
 
 
 export const Shows = () => {
 
-  const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [shows, setShows] = useState<IShow[]>([]);
 
   useEffect(() => {
-    
     axios.get("http://localhost:5000/api/shows/").then(res => {
-      console.log(res);
+      setShows(res.data);
+      setLoading(false);
     });
   }, [])
 
   return (
     <div>
-
+      {!loading ? (
+        shows.map((show) => (
+          <ShowCard
+            key={show._id}
+            id={show._id}
+            name={show.name}
+            genres={show.genres}
+            poster={show.poster}
+          />
+        ))
+      ) : (
+        <CircularProgress />
+      )}
     </div>
-  )
+  );
 }
