9c31c39ccacb6b102d661c96680aa0cb7bac74a2
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-04-27 22:35:54 +1000

    added route to individual show

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import { ShowSearch } from './pages/ShowSearch';
 import { Shows } from './pages/Shows';
+import { Show } from './pages/Show';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/search">
           <ShowSearch />
         </Route>
+        <Route path="/show/:id" component={Show}/>
         <Route exact path="/">
           <Shows />
         </Route>
